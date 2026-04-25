07a5ea0222a07d53e36f8991d063c0e5a0fe5a05
Isha Khatana <ishakhatana17@gmail.com>
2026-04-25 13:57:44 +0530

    Updated server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from openenv.core import create_fastapi_app
-from environment import BharatBuildsEnv
+from environment import BharatBuildsEnv, BharatAction, BharatObservation
 from verifiers import run_all_verifiers
 
-app = create_fastapi_app(BharatBuildsEnv)
+app = create_fastapi_app(BharatBuildsEnv, BharatAction, BharatObservation)
